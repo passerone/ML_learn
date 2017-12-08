b0822e5bdc122bfd033bd13e5fac22b12caefc6f
Hooper Lon <kokia233@126.com>
2017-12-08 21:20:39 +0800

    still on wroking

--- fivepotthree.py
+++ fivepotthree.py
@@ -41,104 +41,48 @@ def datachange(datamat):
     data=datamat[:][1:n]
     return mat(data)
 
+
+
 class NeuralNetwork:
-    def __init__(self,layers):
+    def __init__(self,layers,learning_rate=1):
         '''
         :param layers: list [input num,hide num,output num]
         :param activation: activation funtion
         '''
+        self.input_nodes = layers[0]
+        self.hidden_nodes = layers[1]
+        self.output_nodes = layers[2]
 
-        self.layers = layers
-        self.num_layers = len(layers)
+        self.weight_i2h = random.rand((self.input_nodes,self.hidden_nodes))
+        self.weight_h2o = random.rand((self.hidden_nodes,self.output_nodes))
+        self.lr = learning_rate
 
+    def train(self,inputset,labels):
+        #forword pass
+        #hidden layer
+        hidden_inputs = dot(self.weight_i2h,inputset)
+        hidden_outputs = sigmoid(hidden_inputs)
+        #output layer
+        final_inputs = dot(self.weight_h2o,hidden_outputs)
+        final_outputs = sigmoid(final_inputs)
 
+        #backward pass
 
-class Neuron:
-    def __init__(self,len_input):
-        self.weight=random.rand((len_input,1))*0.1
-        self.offset=0
+        output_errors = labels - final_outputs
+        hidden_errors = dot(output_errors,self.weight_h2o)
 
-        self.input = np.ones(len_input)
-        # 对下一层的输出值
-        self.output = 1
-        # 误差项
-        self.deltas_item = 0
+        #update weight
+        self.weight_h2o += output_errors*hidden_outputs.T*self.lr
+        self.weight_i2h += (inputset*hidden_errors*self.lr).T
 
-    def get_back_weight(self):
-        # 获取反馈差值
-        return self.weights * self.deltas_item
+    def predict(self,inputlist):
+        #forward pass
+        # hidden layer
+        hidden_inputs = dot(self.weight_i2h, inputset)
+        hidden_outputs = sigmoid(hidden_inputs)
+        # output layer
+        final_inputs = dot(self.weight_h2o, hidden_outputs)
+        final_outputs = sigmoid(final_inputs)
+        return final_outputs
 
-    def update(self,dweight,doffset):
-        self.dweight+=dweight
-        self.offset+=doffset
-
-    def calcout(self,input):
-        output=sigmoid(dot(input,self.weight))
-        return output
-
-class NetLayer:
-    def __init__(self,layer_name,len_node,len_input):
-        '''
-        :param layer_name: 'Hidden' or 'Output'
-        :param len_node:
-        :param len_input:
-        '''
-        self.neurons=[Neuron(len_input) for _ in fange(len_node)] #对于无需关注其实际含义的变量可以用_代替
-        self.next_layer = None   # 记录下一层的引用，方便递归操作
-        self.layer_name = layer_name
-
-    def calcout(self,input):
-        output = array([ node.calcout(input) for node in self.neurons])
-        if self.next_layer is not None:
-            return self.next_layer.calc_output(output)
-        return output
-
-    def get_back_weight(self):
-        return sum([node.get_back_weight() for node in self.neurons])
-
-    def update(self,input,labels,learning_rate):
-        if self.layer_name == 'Output':
-            back_weight = zeros(len(self.neurons))
-        elif self.layer_name == 'Hidden':
-            back_weight = self.next_layer.update(input,labels,learning_rate)
-
-
-
-            dweight = learning_rate*sigmoid(self.calcout(input),True)*(labels-self.calcout(input))*input
-            doffset = -1*learning_rate*sigmoid(self.calcout(input),True)*(labels-self.calcout(input))
-        elif self.layer_name == 'Hidden':
-            dweight = learning_rate*sigmoid(self.calcout(input),True)*(labels-self.calcout(input))*input
-            doffset =
-
-if __name__=='__main__':
-    mydat=loaddata('xiguan30.txt')
-    mydata=datachange(mydat)
-    m,n=shape(mydata)
-    random.seed(1)   #你的随机数设定产生种子是一个良好的习惯。这样一来，你得到的权重初始化集仍是随机分布的，但每次开始训练时，得到的权重初始集分布都是完全一致的。
-    syn0=random.rand(n-1,n)
-    syn1=random.rand(n,1)
-    eta=1
-    a=mat(ones((1,n)))*(-1)
-    nsyn0=vstack((syn0,a))
-    nsyn1=vstack((syn1,a))
-    theta=mat(zeros((m,n)))
-    gamma=mat(zeros((m,1)))    #阈值作为系数为-1的特征带入计算
-
-    l0=hstack((mydata[:,:-1],theta))
-    l1=hstack((sigmoid(dot(l0,syn0)),gamma))
-    l2=sigmoid(dot(l1,syn1))
-
-    l2error=mydata[:,-1]-l2
-    dsyn0=eta*sigmoid(l2,True)*l2error*l1
-    dsyn1=-1*eta*sigmoid(l1,True)*l2_delta.dot(syn1.T)
-    dtheta=-1*eta*sigmoid(l2,True)*l2error*mydata[:,:-1]
-    for i in range(1):
-        syn1[:,j] += dtheta
-    for j in range(n):
-        syn0[:,k] += dgamma
-
-
-
-
-
-
+if __name__ == 
